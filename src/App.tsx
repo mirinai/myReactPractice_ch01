6fcd53151bac85a6b0a5ffa1abff50785895cb8c
mirinai <tmddjs1771@gmail.com>
2024-06-24 19:02:55 +0900

    first learning

---
+++ src/App.tsx
@@ -0,0 +1,42 @@
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.tsx</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+import * as D from './data'
+
+export default function App() {
+  // console.log('App called')
+  // return <h1>Hello world</h1>
+  return (
+    <div>
+      <p>
+        {D.randomName()}, {D.randomJobTitle()}, {D.randomDayMonthYear()}
+      </p>
+      <img src={D.randomAvatar()} height="50" />
+      <img src={D.randomImage()} height="300" />
+    </div>
+  )
+}
